2dea0d21d381bada949ec8a2fddd84192c33624f
fjktkm <fjktkm@gmail.com>
2025-02-21 09:31:24 +0000

    ✨ Update run script to execute patch size scripts for each type

--- run.sh
+++ run.sh
@@ -2,7 +2,10 @@
 
 BASE_DIR="patched_font_transformer/scripts/classification"
 TYPES="style weight"
+PATCH_SIZES="1 2 4 8 16"
 
 for type in $TYPES; do
-    uv run "$BASE_DIR/$type.py"
+    for patch_size in $PATCH_SIZES; do
+        uv run "$BASE_DIR/$type/patch_size_$patch_size.py"
+    done
 done
